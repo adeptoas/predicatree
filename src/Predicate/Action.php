a461cdd00616f2142930508dfcf221d00b5cd606
Karim Djemai <karimdjemai1997@gmail.com>
2018-10-26 14:32:04 +0200

    Enable user to submit custom action classes

--- src/Predicate/Action.php
+++ src/Predicate/Action.php
@@ -10,9 +10,19 @@
 		];
 
 		public static function fromSpecifiedArray(array $data): Action {
-			$action = strtolower($data['action']);
-			$class = __NAMESPACE__ . '\\Action\\' . ucfirst(FancyString::toCamelCase($action)) . 'Action';
-
+			if (class_exists($data['action'])) {
+				//user submitted own action
+				$class = $data['action'];
+				$reflection = new \ReflectionClass($class);
+				
+				if ($reflection->isSubclassOf(self::class)) {
+					throw new \BadMethodCallException('Action class must extend ' . self::class);
+				}
+			} else {
+				$action = strtolower($data['action']);
+				$class = __NAMESPACE__ . '\\Action\\' . ucfirst(FancyString::toCamelCase($action)) . 'Action';
+			}
+			
 			$args = $data['arguments'];
 
 			return new $class(...$args);
