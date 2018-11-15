4da2282a857e57cdfb5b11c5a9d1035b1b60978a
Karim Djemai <karimdjemai1997@gmail.com>
2018-11-15 15:28:53 +0100

    throw BadFunctionCall if the ConditionClass doesn't Exist

--- src/Predicate/Condition.php
+++ src/Predicate/Condition.php
@@ -2,7 +2,8 @@
 	namespace Adepto\PredicaTree\Predicate;
 
 	use Adepto\Fancy\FancyString;
-
+	use BadFunctionCallException;
+	
 	abstract class Condition extends CacheObject {
 		const BASE_SPECIFICATION = [
 			'condition'	=>	'string!',
@@ -11,7 +12,9 @@
 
 		public static function fromSpecifiedArray(array $data): Condition {
 			$operator = strtolower($data['condition']);
-			$class = __NAMESPACE__ . '\\Condition\\' . ucfirst(FancyString::toCamelCase($operator)) . 'Condition';
+			if (!class_exists($class = __NAMESPACE__ . '\\Condition\\' . ucfirst(FancyString::toCamelCase($operator)) . 'Condition')) {
+				throw new BadFunctionCallException($class . ' is not a condition');
+			}
 
 			$args = $data['arguments'];
 
